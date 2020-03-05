7381e6b4c7ed36c39836f8a55367acfb611bef17
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-05 19:01:33 +0100

    display basic framway's infos display detailed infos on current configuration for styles

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,49 @@
 var Dashboard = Object.getPrototypeOf(app).Dashboard = new Component("dashboard");
-Dashboard.debug = true;
+// Dashboard.debug = true;
 Dashboard.createdAt      = "2.0.0";
 Dashboard.lastUpdate     = "2.0.0";
 Dashboard.version        = "1";
-// Dashboard.factoryExclude = true;
+Dashboard.factoryExclude = true;
 // Dashboard.loadingMsg     = "This message will display in the console when component will be loaded.";
 
-// Dashboard.prototype.onCreate = function(){
-  // do thing after element's creation
-// }
+Dashboard.prototype.onCreate = function(){
+	var dashboard = this;
+
+	var strComponents = '';
+	for(var component of app.components)
+		strComponents += '<li>'+utils.getClassName(component)+'</li>';
+	var strThemes = '';
+	for(var theme of app.themes)
+		strThemes += '<li>'+theme+'</li>';
+
+
+	var $body = $(require('mustache-loader!html-loader?interpolate!./templates/index.html')({
+		app: app,
+		components: strComponents,
+		themes: strThemes,
+		config: getConfigStr(app.styles, 'global')
+	}));
+
+	dashboard.$el.append($body);
+}
+
+function getConfigStr(obj,title = ''){
+	var template = require('mustache-loader?noShortcut!html-loader?interpolate!./templates/config_section.html');  // noShortcut is used to insert partials later into the final template
+	var rows = ''; // the partials mentionned above
+	var arrObjects = {}; // used to store and process later the sub-object of config
+	var htmlStack = ''; // used to stack the multiple results form arrObjects results
+	$.each(obj,function(key,value){
+		if(typeof value != 'object'){
+			var str = '<span class="ellipsis" title="'+key+'">'+key+' :</span><span>'+value+'</span>';
+			if(value.indexOf('#') != -1)
+				str = '<span class="ellipsis" title="'+key+'">'+key+' :</span><span class="bd-bottom-'+utils.getObjKeyByValue(app.styles.colors, value)+'-5 p-bottom-0">'+value+'</span>';
+			rows += require('mustache-loader!html-loader?interpolate!./templates/config_row.html')({str: str});
+		}
+		else
+			arrObjects[key] = value;
+	});
+	$.each(arrObjects,function(key,value){
+		htmlStack += getConfigStr(value, key);
+	});
+	return template.render({title: title},{rows: rows})+htmlStack; // return the initial template filled with his rows PLUS the stack we get by processing recursively the config
+};
