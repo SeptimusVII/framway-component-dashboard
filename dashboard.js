2d2c29d336012a347d6d5d43cacad79b81e8dbef
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-03-05 15:52:12 +0100

    Initial commit

---
+++ dashboard.js
@@ -0,0 +1,11 @@
+var Dashboard = Object.getPrototypeOf(app).Dashboard = new Component("dashboard");
+Dashboard.debug = true;
+Dashboard.createdAt      = "2.0.0";
+Dashboard.lastUpdate     = "2.0.0";
+Dashboard.version        = "1";
+// Dashboard.factoryExclude = true;
+// Dashboard.loadingMsg     = "This message will display in the console when component will be loaded.";
+
+// Dashboard.prototype.onCreate = function(){
+  // do thing after element's creation
+// }
